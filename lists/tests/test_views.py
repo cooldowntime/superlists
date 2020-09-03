158663d1b5a68b578ffb98d902e024014a063c30
jinenglengque <2441709716@qq.com>
2020-09-04 00:32:19 +0800

    Split out unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+
 from lists.models import Item, List
 
 
@@ -11,31 +12,6 @@ class HomePageTest(TestCase):
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
-
-class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        _list = List()
-        _list.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = _list
-        first_item.save()
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = _list
-        second_item.save()
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, _list)
-
-        saved_items = Item.objects.all()
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, _list)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, _list)
 
 
 class ListViewTest(TestCase):
