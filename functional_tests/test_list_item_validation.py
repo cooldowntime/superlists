7cb9c6540e7172479bba93b6814b04e29c224782
jinenglengque <2441709716@qq.com>
2020-09-05 20:44:02 +0800

    implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -39,3 +39,17 @@ class ItemValidationTest(FunctionTest):
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
 
+    def test_cannot_add_duplicate_items(self):
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Buy wallies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy wallies')
+
+        # 输入重复的事项
+        self.get_item_input_box().send_keys('Buy wallies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You've already got this in your list"
+        ))
+
--- lists/views.py
+++ lists/views.py
@@ -9,21 +9,21 @@ def home_page(request):
 
 
 def view_list(request, list_id):
-    _list = List.objects.get(id=list_id)
+    list_ = List.objects.get(id=list_id)
     form = ItemForm()
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=_list)
-            return redirect(_list)
-    return render(request, 'list.html', {'list': _list, "form": form})
+            form.save(for_list=list_)
+            return redirect(list_)
+    return render(request, 'list.html', {'list': list_, "form": form})
 
 
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {'form': form})
